4c16e17ff1a4f31e9de5299236610c5f80866741
mush1200 <mush120@hotmail.com>
2021-06-30 15:25:06 +0800

    顯示動態資料

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 
+const movieOne = {
+  id: 1,
+  title: 'Jurassic World: Fallen Kingdom',
+  image: 'https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg',
+}
+
 app.use(express.static('public'))
 
 app.engine('handlebars', exphbs({defaultlayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { movie: movieOne})
 })
 
 app.listen(port, () => {
